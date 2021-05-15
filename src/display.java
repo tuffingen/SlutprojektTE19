7b652d0958d70d90065d20caa614bfb47061bf82
tuffingen <60388902+tuffingen@users.noreply.github.com>
2021-05-15 11:01:44 +0200

    Random apple spawns

--- src/display.java
+++ src/display.java
@@ -1,6 +1,7 @@
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
+import java.util.Random;
 
 
 public class display extends JPanel implements ActionListener {
@@ -16,20 +17,35 @@ public class display extends JPanel implements ActionListener {
     int applesEaten;
     int appleX;
     int appleY;
-
+    char direction = 'R';
+    boolean running = false;
+    Timer  timer;
+    Random random;
 
 
     display(){
-
+        random = new Random();
+        this.setPreferredSize(new Dimension(SCREEN_WIDTH,SCREEN_HEIGHT));
+        this.setBackground(Color.green);
+        this.setFocusable(true);
+        this.addKeyListener(new MyKeyAdapter());
+        startGame();
     }
     public void startGame(){
-
+        newApple();
+        running = true;
+        timer = new Timer(DELAY,this);
+        timer.start();
     }
     public void PaintComp(Graphics g){
-
+        super.paintComponent(g);
     }
     public void draw(Graphics g){
 
+    }
+    public void newApple(){
+        appleX = random.nextInt((int)(SCREEN_WIDTH/UNIT_SIZE))*UNIT_SIZE;
+        appleY = random.nextInt((int)(SCREEN_HEIGHT/UNIT_SIZE))*UNIT_SIZE;
     }
     public void move(){
 
