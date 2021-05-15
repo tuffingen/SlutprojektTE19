bd61aa24e19d7a30bd36d672e4a645cb30ce5d03
tuffingen <60388902+tuffingen@users.noreply.github.com>
2021-05-15 10:52:15 +0200

    Array with all the body parts and set a screen size

--- src/display.java
+++ src/display.java
@@ -5,6 +5,19 @@ import java.awt.event.*;
 
 public class display extends JPanel implements ActionListener {
 
+    static final int SCREEN_WIDTH = 600;
+    static final int SCREEN_HEIGHT = 600;
+    static final int UNIT_SIZE = 25;
+    static final int GAME_UNITS = (SCREEN_WIDTH*SCREEN_HEIGHT)/UNIT_SIZE;
+    static final int  DELAY = 75;
+    final int x[] = new int[GAME_UNITS];
+    final int y[] = new int[GAME_UNITS];
+    int bodyParts = 6;
+    int applesEaten;
+    int appleX;
+    int appleY;
+
+
 
     display(){
 
