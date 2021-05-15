1cdd4ebdf2a2b1a9543ad609193a8c9c88bfa6a6
tuffingen <60388902+tuffingen@users.noreply.github.com>
2021-05-15 10:48:05 +0200

    Added classes for all the components in the game

--- src/display.java
+++ src/display.java
@@ -1,14 +1,45 @@
 import javax.swing.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.*;
+import java.awt.event.*;
+
 
 public class display extends JPanel implements ActionListener {
 
+
     display(){
+
+    }
+    public void startGame(){
+
+    }
+    public void PaintComp(Graphics g){
+
+    }
+    public void draw(Graphics g){
+
+    }
+    public void move(){
+
+    }
+    public void checkpoint(){
+    }
+    public void checkCollisions(){
+
+    }
+    public void gameOver(){
 
     }
     @Override
     public void actionPerformed(ActionEvent e) {
 
     }
+
+    public class MyKeyAdapter extends KeyAdapter{
+        @Override
+        public void keyPressed(KeyEvent e){
+
+        }
+    }
+
+
 }
